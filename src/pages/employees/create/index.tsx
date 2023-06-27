348793a3c79a1c3134c5a5937faa6d5d360a915b
roq-ai <>
2023-06-27 21:25:13 +0000

    Initial commit

---
+++ src/pages/employees/create/index.tsx
@@ -0,0 +1,132 @@
+import AppLayout from 'layout/app-layout';
+import React, { useState } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Text,
+  Box,
+  Spinner,
+  FormErrorMessage,
+  Switch,
+  NumberInputStepper,
+  NumberDecrementStepper,
+  NumberInputField,
+  NumberIncrementStepper,
+  NumberInput,
+} from '@chakra-ui/react';
+import { useFormik, FormikHelpers } from 'formik';
+import * as yup from 'yup';
+import DatePicker from 'react-datepicker';
+import { FiEdit3 } from 'react-icons/fi';
+import { useRouter } from 'next/router';
+import { createEmployee } from 'apiSdk/employees';
+import { Error } from 'components/error';
+import { employeeValidationSchema } from 'validationSchema/employees';
+import { AsyncSelect } from 'components/async-select';
+import { ArrayFormField } from 'components/array-form-field';
+import { AccessOperationEnum, AccessServiceEnum, withAuthorization } from '@roq/nextjs';
+import { UserInterface } from 'interfaces/user';
+import { OrganizationInterface } from 'interfaces/organization';
+import { getUsers } from 'apiSdk/users';
+import { getOrganizations } from 'apiSdk/organizations';
+import { EmployeeInterface } from 'interfaces/employee';
+
+function EmployeeCreatePage() {
+  const router = useRouter();
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (values: EmployeeInterface, { resetForm }: FormikHelpers<any>) => {
+    setError(null);
+    try {
+      await createEmployee(values);
+      resetForm();
+      router.push('/employees');
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const formik = useFormik<EmployeeInterface>({
+    initialValues: {
+      energy_level: 0,
+      user_id: (router.query.user_id as string) ?? null,
+      organization_id: (router.query.organization_id as string) ?? null,
+    },
+    validationSchema: employeeValidationSchema,
+    onSubmit: handleSubmit,
+    enableReinitialize: true,
+    validateOnChange: false,
+    validateOnBlur: false,
+  });
+
+  return (
+    <AppLayout>
+      <Box bg="white" p={4} rounded="md" shadow="md">
+        <Box mb={4}>
+          <Text as="h1" fontSize="2xl" fontWeight="bold">
+            Create Employee
+          </Text>
+        </Box>
+        {error && (
+          <Box mb={4}>
+            <Error error={error} />
+          </Box>
+        )}
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl id="energy_level" mb="4" isInvalid={!!formik.errors?.energy_level}>
+            <FormLabel>Energy Level</FormLabel>
+            <NumberInput
+              name="energy_level"
+              value={formik.values?.energy_level}
+              onChange={(valueString, valueNumber) =>
+                formik.setFieldValue('energy_level', Number.isNaN(valueNumber) ? 0 : valueNumber)
+              }
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            {formik.errors.energy_level && <FormErrorMessage>{formik.errors?.energy_level}</FormErrorMessage>}
+          </FormControl>
+          <AsyncSelect<UserInterface>
+            formik={formik}
+            name={'user_id'}
+            label={'Select User'}
+            placeholder={'Select User'}
+            fetcher={getUsers}
+            renderOption={(record) => (
+              <option key={record.id} value={record.id}>
+                {record?.email}
+              </option>
+            )}
+          />
+          <AsyncSelect<OrganizationInterface>
+            formik={formik}
+            name={'organization_id'}
+            label={'Select Organization'}
+            placeholder={'Select Organization'}
+            fetcher={getOrganizations}
+            renderOption={(record) => (
+              <option key={record.id} value={record.id}>
+                {record?.name}
+              </option>
+            )}
+          />
+          <Button isDisabled={formik?.isSubmitting} colorScheme="blue" type="submit" mr="4">
+            Submit
+          </Button>
+        </form>
+      </Box>
+    </AppLayout>
+  );
+}
+
+export default withAuthorization({
+  service: AccessServiceEnum.PROJECT,
+  entity: 'employee',
+  operation: AccessOperationEnum.CREATE,
+})(EmployeeCreatePage);
